ec18ebe92da6160e8c6825df7e5d193c4a0ab4cd
Bhavye Mathur <bhavyemathur@gmail.com>
2024-12-14 22:51:24 -0500

    clone() should now work for 0-dimensional tensors and TensorViews with non-1 step sizes

--- src/tensor/clone.rs
+++ src/tensor/clone.rs
@@ -19,27 +19,42 @@ impl<T: RawDataType> TensorClone<T> for TensorView<T> {
     fn copy_data(&self) -> DataOwned<T> {
         let (mut shape, mut stride) = collapse_contiguous(&self.shape, &self.stride);
 
-        let mut contiguous_stride = *stride.last().unwrap();
-        if contiguous_stride == 1 {
-            contiguous_stride = shape.pop().unwrap();
-            stride.pop();
-        }
+        match stride.last_mut() {
+            Some(&mut mut contiguous_stride) => {
+                // if elements along the last axis are located contiguously,
+                // we can collapse the last dimension and copy contiguous_stride elements at once
+                // collapse_contiguous() call before guarantees only the last dimension (if any) is contiguous
+                if contiguous_stride == 1 {
+                    contiguous_stride = shape.pop().unwrap();
+                    stride.pop();
+                }
+                // if elements along the last axis aren't located contiguously,
+                // they must correspond to a TensorView with a step-size along the last axis of > 1
+                // this is equivalent to 1 contiguous element along the last axis
+                else {
+                    contiguous_stride = 1;
+                }
 
-        let size = self.size();
-        let mut data = Vec::with_capacity(size);
+                let size = self.size();
+                let mut data = Vec::with_capacity(size);
 
-        let src = self.data.const_ptr();
-        let mut dst = data.as_mut_ptr();
+                let src = self.data.const_ptr();
+                let mut dst = data.as_mut_ptr();
 
-        for i in FlatIndexIterator::from(&shape, &stride) {
-            unsafe {
-                copy_nonoverlapping(src.offset(i), dst, contiguous_stride);
-                dst = dst.add(contiguous_stride);
+                for i in FlatIndexIterator::from(&shape, &stride) {
+                    unsafe {
+                        copy_nonoverlapping(src.offset(i), dst, contiguous_stride);
+                        dst = dst.add(contiguous_stride);
+                    }
+                }
+
+                unsafe { data.set_len(size); }
+                DataOwned::new(data)
+            }
+            None => {
+                DataOwned::new(vec![self.data[0]])  // zero-dimensional tensor
             }
         }
-
-        unsafe { data.set_len(size); }
-        DataOwned::new(data)
     }
 }
 
