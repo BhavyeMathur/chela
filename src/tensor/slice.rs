9bc9f1fef5a2acfb63305fe107010d16107fdb05
Bhavye Mathur <bhavyemathur@gmail.com>
2025-01-04 18:42:14 +0530

    Organised slicing code

--- src/tensor/slice.rs
+++ src/tensor/slice.rs
@@ -6,48 +6,55 @@ use crate::iterator::collapse_contiguous::is_contiguous;
 use crate::tensor::flags::TensorFlags;
 use crate::Tensor;
 
+fn update_flags_with_contiguity(mut flags: TensorFlags, shape: &[usize], stride: &[usize]) -> TensorFlags {
+    flags -= TensorFlags::Owned;
+
+    if is_contiguous(&shape, &stride) {
+        flags | TensorFlags::Contiguous
+    } else {
+        flags - TensorFlags::Contiguous
+    }
+}
+
+fn calculate_strided_buffer_length(shape: &[usize], stride: &[usize]) -> usize {
+    // let mut len = 1;
+    // for i in 0..ndims {
+    //     len += stride[i] * (shape[i] - 1);
+    // }
+    //
+    // the following code is equivalent to the above loop
+    shape.iter().zip(stride.iter())
+        .map(|(&axis_length, &axis_stride)| axis_stride * (axis_length - 1))
+        .sum::<usize>() + 1
+}
+
 
 impl<'a, T: RawDataType> Tensor<'a, T> {
     pub fn slice_along<S: Indexer>(&self, axis: Axis, index: S) -> Tensor<T> {
         let axis = axis.0;
 
-        let mut shape = self.shape.clone();
-        let mut stride = self.stride.clone();
+        let mut new_shape = self.shape.clone();
+        let mut new_stride = self.stride.clone();
 
         if index.collapse_dimension() {
-            shape.remove(axis);
-            stride.remove(axis);
-        }
-        else {
-            shape[axis] = index.indexed_length(shape[axis]);
+            new_shape.remove(axis);
+            new_stride.remove(axis);
+        } else {
+            new_shape[axis] = index.indexed_length(new_shape[axis]);
         }
 
         let offset = self.stride[axis] * index.index_of_first_element();
 
-        // let mut len = 1;
-        // for i in 0..ndims {
-        //     len += stride[i] * (shape[i] - 1);
-        // }
-        //
-        // the following code is equivalent to the above loop
-        let len = shape.iter().zip(stride.iter())
-            .map(|(&axis_length, &axis_stride)| axis_stride * (axis_length - 1))
-            .sum::<usize>() + 1;
-
-        let mut flags = self.flags - TensorFlags::Owned;
-        if is_contiguous(&shape, &stride) {
-            flags |= TensorFlags::Contiguous;
-        } else {
-            flags -= TensorFlags::Contiguous;
-        }
+        let len = calculate_strided_buffer_length(&new_shape, &new_stride);
+        let flags = update_flags_with_contiguity(self.flags, &new_shape, &new_stride);
 
         Tensor {
             ptr: unsafe { self.ptr.add(offset) },
             len,
             capacity: len,
 
-            shape,
-            stride,
+            shape: new_shape,
+            stride: new_stride,
             flags,
 
             _marker: self._marker,
@@ -59,52 +66,31 @@ impl<'a, T: RawDataType> Tensor<'a, T> {
         S: Indexer,
         I: IntoIterator<Item=S>,
     {
-        // repeatedly calls the slice_along() method for each element in the index
-        // we keep a track of which axis to slice along using the axis variable
-        // if the dimension of the tensor is preserved during the slice along, we increment the axis
-        // otherwise the axis isn't incremented because the previous dimension has collapsed
-
         let ndims = self.ndims();
         let mut offset = 0;
-        let mut i = 0;
+        let mut axis = 0;
 
         let mut new_shape = Vec::with_capacity(ndims);
         let mut new_stride = Vec::with_capacity(ndims);
 
         for idx in index {
-            offset += self.stride[i] * idx.index_of_first_element();
-
-            if idx.collapse_dimension() {} else {
-                let new_length = idx.indexed_length(self.shape[i]);
-                // TODO what if new_length is 0?
+            if !idx.collapse_dimension() {
+                let new_length = idx.indexed_length(self.shape[axis]);
                 new_shape.push(new_length);
-                new_stride.push(self.stride[i]);
+                new_stride.push(self.stride[axis]);
             }
 
-            i += 1;
+            offset += self.stride[axis] * idx.index_of_first_element();
+            axis += 1;
         }
 
-        for j in i..ndims {
+        for j in axis..ndims {
             new_shape.push(self.shape[j]);
             new_stride.push(self.stride[j]);
         }
 
-        // let mut len = 1;
-        // for i in 0..ndims {
-        //     len += stride[i] * (shape[i] - 1);
-        // }
-        //
-        // the following code is equivalent to the above loop
-        let len = new_shape.iter().zip(new_stride.iter())
-            .map(|(&axis_length, &axis_stride)| axis_stride * (axis_length - 1))
-            .sum::<usize>() + 1;
-
-        let mut flags = self.flags - TensorFlags::Owned;
-        if is_contiguous(&new_shape, &new_stride) {
-            flags |= TensorFlags::Contiguous;
-        } else {
-            flags -= TensorFlags::Contiguous;
-        }
+        let len = calculate_strided_buffer_length(&new_shape, &new_stride);
+        let flags = update_flags_with_contiguity(self.flags, &new_shape, &new_stride);
 
         Tensor {
             ptr: unsafe { self.ptr.add(offset) },
