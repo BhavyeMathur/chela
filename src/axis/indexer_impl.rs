b50d7eee9f4fd59c1e6b34bd659acda5a22b0e87
Bhavye Mathur <bhavyemathur@gmail.com>
2025-01-04 18:30:47 +0530

    Fixed slicing

--- src/axis/indexer_impl.rs
+++ src/axis/indexer_impl.rs
@@ -1,46 +1,46 @@
 use std::ops::{Range, RangeFrom, RangeFull, RangeInclusive, RangeTo, RangeToInclusive};
 
-pub(super) trait IndexerImpl {
-    fn len(&self, axis: usize, shape: &[usize]) -> usize;
+pub(crate) trait IndexerImpl {
+    fn indexed_length(&self, axis_length: usize) -> usize;
 }
 
 impl IndexerImpl for usize {
-    fn len(&self, _axis: usize, _shape: &[usize]) -> usize {
-        0
+    fn indexed_length(&self, axis_length: usize) -> usize {
+        1
     }
 }
 impl IndexerImpl for Range<usize> {
-    fn len(&self, _axis: usize, _shape: &[usize]) -> usize {
+    fn indexed_length(&self, axis_length: usize) -> usize {
         self.end - self.start
     }
 }
 
 impl IndexerImpl for RangeFull {
-    fn len(&self, axis: usize, shape: &[usize]) -> usize {
-        shape[axis]
+    fn indexed_length(&self, axis_length: usize) -> usize {
+        axis_length
     }
 }
 
 impl IndexerImpl for RangeFrom<usize> {
-    fn len(&self, axis: usize, shape: &[usize]) -> usize {
-        shape[axis] - self.start
+    fn indexed_length(&self, axis_length: usize) -> usize {
+        axis_length - self.start
     }
 }
 
 impl IndexerImpl for RangeTo<usize> {
-    fn len(&self, _axis: usize, _shape: &[usize]) -> usize {
+    fn indexed_length(&self, axis_length: usize) -> usize {
         self.end
     }
 }
 
 impl IndexerImpl for RangeInclusive<usize> {
-    fn len(&self, _axis: usize, _shape: &[usize]) -> usize {
+    fn indexed_length(&self, axis_length: usize) -> usize {
         self.end() - self.start() + 1
     }
 }
 
 impl IndexerImpl for RangeToInclusive<usize> {
-    fn len(&self, _axis: usize, _shape: &[usize]) -> usize {
+    fn indexed_length(&self, axis_length: usize) -> usize {
         self.end + 1
     }
 }
--- src/tensor/slice.rs
+++ src/tensor/slice.rs
@@ -8,12 +8,21 @@ use crate::Tensor;
 
 
 impl<'a, T: RawDataType> Tensor<'a, T> {
-    pub fn slice_along<S>(&self, axis: Axis, index: S) -> Tensor<T>
-    where
-        S: Indexer,
-    {
-        let (shape, stride) = index.indexed_shape_and_stride(&axis, &self.shape, &self.stride);
-        let offset = self.stride[axis.0] * index.index_of_first_element();
+    pub fn slice_along<S: Indexer>(&self, axis: Axis, index: S) -> Tensor<T> {
+        let axis = axis.0;
+
+        let mut shape = self.shape.clone();
+        let mut stride = self.stride.clone();
+
+        if index.collapse_dimension() {
+            shape.remove(axis);
+            stride.remove(axis);
+        }
+        else {
+            shape[axis] = index.indexed_length(shape[axis]);
+        }
+
+        let offset = self.stride[axis] * index.index_of_first_element();
 
         // let mut len = 1;
         // for i in 0..ndims {
@@ -55,19 +64,58 @@ impl<'a, T: RawDataType> Tensor<'a, T> {
         // if the dimension of the tensor is preserved during the slice along, we increment the axis
         // otherwise the axis isn't incremented because the previous dimension has collapsed
 
-        // let mut axis = 0;
-        // let mut ndims = self.ndims();
-        let mut result = self.view();
+        let ndims = self.ndims();
+        let mut offset = 0;
+        let mut i = 0;
 
-        // for idx in index {
-        //     result = result.slice_along(Axis(axis), idx.clone());
-        //
-        //     if result.ndims() == ndims {
-        //         axis += 1;
-        //     } else {
-        //         ndims = result.ndims();
-        //     }
+        let mut new_shape = Vec::with_capacity(ndims);
+        let mut new_stride = Vec::with_capacity(ndims);
+
+        for idx in index {
+            offset += self.stride[i] * idx.index_of_first_element();
+
+            if idx.collapse_dimension() {} else {
+                let new_length = idx.indexed_length(self.shape[i]);
+                // TODO what if new_length is 0?
+                new_shape.push(new_length);
+                new_stride.push(self.stride[i]);
+            }
+
+            i += 1;
+        }
+
+        for j in i..ndims {
+            new_shape.push(self.shape[j]);
+            new_stride.push(self.stride[j]);
+        }
+
+        // let mut len = 1;
+        // for i in 0..ndims {
+        //     len += stride[i] * (shape[i] - 1);
         // }
-        result
+        //
+        // the following code is equivalent to the above loop
+        let len = new_shape.iter().zip(new_stride.iter())
+            .map(|(&axis_length, &axis_stride)| axis_stride * (axis_length - 1))
+            .sum::<usize>() + 1;
+
+        let mut flags = self.flags - TensorFlags::Owned;
+        if is_contiguous(&new_shape, &new_stride) {
+            flags |= TensorFlags::Contiguous;
+        } else {
+            flags -= TensorFlags::Contiguous;
+        }
+
+        Tensor {
+            ptr: unsafe { self.ptr.add(offset) },
+            len,
+            capacity: len,
+
+            shape: new_shape,
+            stride: new_stride,
+            flags,
+
+            _marker: self._marker,
+        }
     }
 }
