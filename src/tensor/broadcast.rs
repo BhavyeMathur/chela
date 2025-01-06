240b4fe047e4bbba197e4c5c445a5be5090c713b
Bhavye Mathur <bhavyemathur@gmail.com>
2025-01-07 03:36:46 +0530

    broadcast_shape and broadcast_stride functions

--- src/tensor/broadcast.rs
+++ src/tensor/broadcast.rs
@@ -2,116 +2,94 @@ use crate::dtype::RawDataType;
 use crate::traits::to_vec::ToVec;
 use crate::Tensor;
 
-fn pad_dimensions(shape: &[usize], stride: &[usize], ndims: usize) -> (Vec<usize>, Vec<usize>) {
-    let mut new_shape = Vec::with_capacity(ndims);
-    let mut new_stride = Vec::with_capacity(ndims);
-
-    for _ in 0..(ndims - shape.len()) {
-        new_shape.push(1);
-        new_stride.push(0);
-    }
-
-    new_shape.extend(shape);
-    new_stride.extend(stride);
-
-    (new_shape, new_stride)
-}
-
 impl<'a, T: RawDataType> Tensor<'a, T> {
     pub fn broadcast_to(&'a self, shape: impl ToVec<usize>) -> Tensor<'a, T> {
         let shape = shape.to_vec();
-        let ndims = shape.len();
-        assert!(ndims >= self.ndims(), "cannot broadcast to fewer dimensions");
 
-        let (mut broadcast_shape, mut broadcast_stride) = pad_dimensions(&self.shape, &self.stride, ndims);
-
-        for axis in 0..ndims {
-            if shape[axis] == broadcast_shape[axis] {
-                continue;
-            }
-
-            if broadcast_shape[axis] != 1 {
-                panic!("tensor is not compatible with the desired shape");
-            }
-
-            broadcast_shape[axis] = shape[axis];
-            broadcast_stride[axis] = 0;
-        }
+        let broadcast_shape = broadcast_shape(&self.shape, shape);
+        let broadcast_stride = broadcast_stride(&self.stride, &broadcast_shape, &self.shape);
 
         unsafe { self.reshaped_view(broadcast_shape, broadcast_stride) }
     }
 }
 
+fn pad<T: Copy>(arr: &[T], value: T, n: usize) -> Vec<T> {
+    let mut new_arr = Vec::with_capacity(arr.len() + n);
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    for _ in 0..n {
+        new_arr.push(value);
+    }
+    new_arr.extend(arr);
+    new_arr
+}
 
-    #[test]
-    fn test_broadcast() {
-        let tensor = Tensor::from([1, 2, 3]);
-        let tensor = tensor.broadcast_to([3, 3]);
-        assert_eq!(tensor.shape(), [3, 3]);
-        assert_eq!(tensor, Tensor::from([[1, 2, 3], [1, 2, 3], [1, 2, 3]]));
+fn pad_dimensions(shape: &[usize], stride: &[usize], ndims: usize) -> (Vec<usize>, Vec<usize>) {
+    let n = ndims - shape.len();
+    let shape = pad(shape, 1, n);
+    let stride = pad(stride, 0, n);
+
+    (shape, stride)
+}
+
+fn broadcast_shape(shape: &[usize], to: impl ToVec<usize>) -> Vec<usize> {
+    let to = to.to_vec();
+    let ndims = to.len();
+
+    if ndims < shape.len() {
+        panic!("cannot broadcast to fewer dimensions")
     }
 
-    #[test]
-    fn test_broadcast_scalar_to_higher_dims() {
-        let tensor = Tensor::from([42]);
-        let tensor = tensor.broadcast_to([2, 3]);
-        assert_eq!(tensor.shape(), [2, 3]);
-        assert_eq!(tensor, Tensor::from([[42, 42, 42], [42, 42, 42]]));
+    // TODO we can just copy shape and return that since broadcast_shape == shape at the end
+    // just need to check that the broadcasting is compatible
+
+    let mut broadcast_shape = pad(shape, 1, ndims - shape.len());
+
+    for axis in 0..ndims {
+        if to[axis] == broadcast_shape[axis] {
+            continue;
+        }
+
+        if broadcast_shape[axis] != 1 {
+            panic!("broadcast is not compatible with the desired shape");
+        }
+
+        broadcast_shape[axis] = to[axis];
     }
 
-    #[test]
-    fn test_broadcast_matrix_to_higher_dims() {
-        let tensor = Tensor::from([[1, 2], [3, 4]]);
-        let tensor = tensor.broadcast_to([3, 2, 2]);
-        assert_eq!(tensor.shape(), [3, 2, 2]);
-        assert_eq!(tensor, Tensor::from([[[1, 2], [3, 4]], [[1, 2], [3, 4]], [[1, 2], [3, 4]]]));
+    broadcast_shape
+}
+
+fn broadcast_stride(stride: &[usize], broadcast_shape: &[usize], original_shape: &[usize]) -> Vec<usize> {
+    let ndims = broadcast_shape.len();
+    let original_first_axis = ndims - original_shape.len();
+
+    if original_first_axis < 0 {
+        panic!("cannot broadcast to fewer dimensions")
     }
 
-    #[test]
-    #[should_panic]
-    fn test_broadcast_incompatible_shapes() {
-        let tensor = Tensor::from([1, 2, 3]);
-        tensor.broadcast_to([3, 5]);
+    let mut broadcast_stride = Vec::with_capacity(ndims);
+
+    for _ in 0..original_first_axis {
+        broadcast_stride.push(0);  // new dimensions get a zero stride
     }
 
-    #[test]
-    fn test_broadcast_identity() {
-        let tensor = Tensor::from([1, 2, 3]);
-        let tensor = tensor.broadcast_to([3]);
-        assert_eq!(tensor.shape(), [3]);
-        assert_eq!(tensor, Tensor::from([1, 2, 3]));
+    for axis in original_first_axis..ndims {
+        let original_axis_length = original_shape[axis - original_first_axis];
+
+        if original_axis_length == 1 {
+            broadcast_stride.push(0);
+        } else if original_axis_length == broadcast_shape[axis] {
+            broadcast_stride.push(stride[axis - original_first_axis]);
+        } else {
+            panic!("broadcast is not compatible with the desired shape");
+        }
     }
 
-    #[test]
-    fn test_broadcast_unchanged() {
-        let tensor = Tensor::from([1, 2, 3]);
-        let tensor = tensor.broadcast_to([1, 3]);
-        assert_eq!(tensor.shape(), [1, 3]);
-        assert_eq!(tensor, Tensor::from([[1, 2, 3]]));
-    }
+    broadcast_stride
+}
 
-    #[test]
-    fn test_broadcast_high_dims() {
-        let tensor = Tensor::from([[1], [2], [3]]);
-        let tensor = tensor.broadcast_to([3, 3, 3]);
-        assert_eq!(tensor.shape(), [3, 3, 3]);
-        assert_eq!(tensor, Tensor::from(
-            [[[1, 1, 1], [2, 2, 2], [3, 3, 3]],
-                [[1, 1, 1], [2, 2, 2], [3, 3, 3]],
-                [[1, 1, 1], [2, 2, 2], [3, 3, 3]]]
-        )
-        );
-    }
+// TODO tests for broadcast_stride()
 
-    #[test]
-    fn test_broadcast_single_dimensional_expansion() {
-        let tensor = Tensor::from([1, 2, 3]);
-        let tensor = tensor.broadcast_to([3, 1, 3]);
-        assert_eq!(tensor.shape(), [3, 1, 3]);
-        assert_eq!(tensor, Tensor::from([[[1, 2, 3]], [[1, 2, 3]], [[1, 2, 3]]]));
-    }
+fn broadcast_shapes(first: &[usize], second: &[usize]) -> Vec<usize> {
+    todo!()
 }
