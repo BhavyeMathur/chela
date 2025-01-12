50e8c020687ef2c249fefe09c0510480fd68c43f
Aman-Amith-Shastry <amanshastrychess@gmail.com>
2025-01-12 10:14:14 -0500

    Removed cpp run and compile functions

--- benches/perfprofiler/util.py
+++ benches/perfprofiler/util.py
@@ -15,11 +15,6 @@ def compile_rust(target: str) -> str:
     out = out.split(b"\n")[-3]
     return json.loads(out)["executable"]
 
-def compile_cpp(target: str) -> str:
-    out = subprocess.check_output(f"")
-    out = out.split(b"\n")[-3]
-    return json.loads(out)["executable"]
-
 def get_method_class(method) -> str:
     return method.__qualname__.split(".")[0]
 
