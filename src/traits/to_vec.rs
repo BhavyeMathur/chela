22444364bf4f821f7002c15f0fdcff35f4a1fef1
Aman-Amith-Shastry <amanshastrychess@gmail.com>
2025-01-20 22:34:48 -0500

    Performance testing for sum across NumPy, PyTorch, and Chela.

--- src/traits/to_vec.rs
+++ src/traits/to_vec.rs
@@ -2,29 +2,27 @@ use crate::axes_traits::AxisType;
 use crate::Axis;
 
 pub(crate) trait ToVec<T> {
-    fn to_vec(&self) -> Vec<T>;
+    fn to_vec(self) -> Vec<T>;
 }
 
 impl ToVec<usize> for usize {
-    fn to_vec(&self) -> Vec<usize> {
-        vec![*self]
+    fn to_vec(self) -> Vec<usize> {
+        vec![self]
     }
 }
 
 impl ToVec<usize> for Axis {
-    fn to_vec(&self) -> Vec<usize> {
+    fn to_vec(self) -> Vec<usize> {
         vec![self.usize()]
     }
 }
 
 impl<T> ToVec<T> for Vec<T> {
-    fn to_vec(&self) -> Vec<T> {
-        self.into_vec()
-    }
+    fn to_vec(self) -> Vec<T> { self }
 }
 
 impl<T, const N: usize> ToVec<T> for [T; N] {
-    fn to_vec(&self) -> Vec<T> {
+    fn to_vec(self) -> Vec<T> {
         Vec::from(self)
     }
 }
