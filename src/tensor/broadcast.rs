a246161371d1f8293f075b2f9e0e1ba1fef4f4a0
Bhavye Mathur <bhavyemathur@gmail.com>
2025-01-20 22:32:28 -0500

    broadcast_shapes() method

--- src/tensor/broadcast.rs
+++ src/tensor/broadcast.rs
@@ -14,9 +14,9 @@ impl<'a, T: RawDataType> Tensor<'a, T> {
 }
 
 fn pad<T: Copy>(arr: &[T], value: T, n: usize) -> Vec<T> {
-    let mut new_arr = Vec::with_capacity(arr.len() + n);
+    let mut new_arr = Vec::with_capacity(n);
 
-    for _ in 0..n {
+    for _ in 0..(n - arr.len()) {
         new_arr.push(value);
     }
     new_arr.extend(arr);
@@ -81,5 +81,42 @@ fn broadcast_stride(stride: &[usize], broadcast_shape: &[usize], original_shape:
 // TODO tests for broadcast_stride()
 
 fn broadcast_shapes(first: &[usize], second: &[usize]) -> Vec<usize> {
-    todo!()
+    let mut shape1;
+    let mut shape2;
+
+    if first.len() > second.len() {
+        shape1 = pad(second, 1, first.len());
+        shape2 = first.to_vec();
+    } else {
+        shape1 = pad(first, 1, second.len());
+        shape2 = second.to_vec();
+    }
+
+    for axis in 0..shape1.len() {
+        if shape1[axis] == 1 {
+            shape1[axis] = shape2[axis];
+        } else if shape2[axis] == 1 {
+            shape2[axis] = shape1[axis];
+        } else if shape1[axis] != shape2[axis] {
+            panic!("broadcast is not compatible with the desired shape");
+        }
+    }
+
+    shape1
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::broadcast::broadcast_shapes;
+
+    #[test]
+    fn test_broadcast_shapes() {
+        let shape1 = vec![5, 1];
+        let shape2 = vec![2, 1, 3];
+
+        let correct = vec![2, 5, 3];
+        let output = broadcast_shapes(&shape1, &shape2);
+
+        assert_eq!(output, correct);
+    }
 }
