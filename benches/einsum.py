8a70d6745ea7998f69075a30b77a137adfc53522
Bhavye Mathur <bhavyemathur@gmail.com>
2025-05-15 11:52:51 -0500

    More einsum benchmarks

--- benches/einsum.py
+++ benches/einsum.py
@@ -3,33 +3,127 @@ import torch
 
 from perfprofiler import *
 
-class TensorSum(TimingSuite):
-    def __init__(self, n):
-        self.n = n
 
-        i = int(10)
-        j = int(100)
-        k = int(1000)
-        m = int(50)
+class TensorEinsumBase(TimingSuite):
+    ID: int
 
-        self.ndarray_a: np.ndarray = np.random.rand(i, j).astype(np.float32)
-        self.ndarray_b: np.ndarray = np.random.rand(k, m).astype(np.float32)
+    def __init__(self, einsum_string: str, shapes: dict[str, int]):
+        self.einsum_string = einsum_string
 
-        # self.tensor_a_cpu = torch.rand((i, k), dtype=torch.float32)
-        # self.tensor_b_cpu = torch.rand((j, k), dtype=torch.float32)
+        shapes = self.tensor_dims_from_einsum_string(einsum_string, shapes)
+        self.ndarrays = rand_ndarrays_with_shape(shapes)
+        self.tensors = rand_tensors_with_shape(shapes)
+
+    @classmethod
+    def tensor_dims_from_einsum_string(cls, einsum_string: str, shapes: dict[str, int]) -> list[tuple[int, ...]]:
+        einsum_string = einsum_string.split("->")[0]
+        labels = einsum_string.split(",")
+        return [tuple(shapes[char] for char in label) for label in labels]
+
+    @measure_performance("PyTorch CPU")
+    def run(self):
+        torch.einsum(self.einsum_string, *self.tensors)
 
     @measure_performance("NumPy")
     def run(self):
-        np.einsum("ij,km->im", self.ndarray_a, self.ndarray_b)
-
-    # @measure_performance("PyTorch CPU")
-    # def run(self):
-    #     torch.einsum("ik,jk->ij", self.tensor_a_cpu, self.tensor_b_cpu)
+        np.einsum(self.einsum_string, *self.ndarrays)
 
     @measure_rust_performance("Chela CPU", target="einsum")
     def run(self, executable):
-        return self.run_rust(executable, self.n)
+        return run_rust(executable, self.ID)
+
+
+class TensorEinsum1(TensorEinsumBase):
+    ID = 1
+    name = "Dot Product"
+
+    def __init__(self):
+        super().__init__("i,i->",
+                         {"i": 10000})
+
+
+class TensorEinsum2(TensorEinsumBase):
+    ID = 2
+    name = "Matrix-Vector"
+
+    def __init__(self):
+        super().__init__("ij,j->i",
+                         {"i": 1000, "j": 500})
+
+
+class TensorEinsum3(TensorEinsumBase):
+    ID = 3
+    name = "Matrix Mult"
+
+    def __init__(self):
+        super().__init__("ij,jk->ik",
+                         {"i": 100, "j": 1000, "k": 500})
+
+
+class TensorEinsum4(TensorEinsumBase):
+    ID = 4
+    name = "Outer Product"
+
+    def __init__(self):
+        super().__init__("ik,jk->ij",
+                         {"i": 100, "j": 1000, "k": 500})
+
+
+class TensorEinsum5(TensorEinsumBase):
+    ID = 5
+    name = "3 Operands"
+
+    def __init__(self):
+        super().__init__("abc,bd,de->ae",
+                         {"a": 100, "b": 5, "c": 20, "d": 50, "e": 100})
+
+
+class TensorEinsum6(TensorEinsumBase):
+    ID = 6
+    name = "4 Operands"
+
+    def __init__(self):
+        super().__init__("abc,bd,bc,ce->ae",
+                         {"a": 100, "b": 5, "c": 20, "d": 50, "e": 100})
+
+
+class TensorEinsum7(TensorEinsumBase):
+    ID = 7
+    name = "Broadcasting"
+
+    def __init__(self):
+        super().__init__("ij,kj->ikj",
+                         {"i": 128, "j": 64, "k": 32})
+
+
+class TensorEinsum8(TensorEinsumBase):
+    ID = 8
+    name = "Sum"
+
+    def __init__(self):
+        super().__init__("abc->",
+                         {"a": 100, "b": 100, "c": 100})
+
+
+class TensorEinsum9(TensorEinsumBase):
+    ID = 9
+    name = "Diagonal"
+
+    def __init__(self):
+        super().__init__("ii->i",
+                         {"i": 1000})
+
+
+class TensorEinsum10(TensorEinsumBase):
+    ID = 10
+    name = "Reshape"
+
+    def __init__(self):
+        super().__init__("abcd->dcba",  # Axis reversal with shape retention
+                         {"a": 10, "b": 20, "c": 30, "d": 40})
+
 
 if __name__ == "__main__":
-    results = TensorSum.profile_each([0], n=10)
-    print(results)
+    results = profile_all([TensorEinsum1, TensorEinsum2, TensorEinsum3, TensorEinsum4, TensorEinsum5,
+                           TensorEinsum6, TensorEinsum7, TensorEinsum8, TensorEinsum9, TensorEinsum10], n=10)
+    plot_barplot(results, "Einstein Summation Benchmark")
--- benches/einsum.rs
+++ benches/einsum.rs
@@ -1,22 +1,155 @@
 use chela::*;
+use std::env;
 
 use cpu_time::ProcessTime;
 
 
-fn profile() -> u128 {
-    let i = 10;
-    let j = 100;
-    let k = 1000;
-    let m = 50;
+fn einsum1() -> u128 {
+    let i = 10000;
 
-    let tensor_a: Tensor<f32> = Tensor::rand([i, j]);
-    let tensor_b: Tensor<f32> = Tensor::rand([k, m]);
+    let tensor_a: Tensor<f32> = Tensor::rand([i]);
+    let tensor_b: Tensor<f32> = Tensor::rand([i]);
 
     let start = ProcessTime::now();
-    _ = einsum(&[&tensor_a, &tensor_b], (["ij", "km"], "im"));
+    _ = einsum(&[&tensor_a, &tensor_b], (["i", "i"], ""));
+    start.elapsed().as_nanos()
+}
+
+fn einsum2() -> u128 {
+    let i = 1000;
+    let j = 500;
+
+    let tensor_a: Tensor<f32> = Tensor::rand([i, j]);
+    let tensor_b: Tensor<f32> = Tensor::rand([j]);
+
+    let start = ProcessTime::now();
+    _ = einsum(&[&tensor_a, &tensor_b], (["ij", "j"], "i"));
+    start.elapsed().as_nanos()
+}
+
+fn einsum3() -> u128 {
+    let i = 100;
+    let j = 1000;
+    let k = 500;
+
+    let tensor_a: Tensor<f32> = Tensor::rand([i, j]);
+    let tensor_b: Tensor<f32> = Tensor::rand([j, k]);
+
+    let start = ProcessTime::now();
+    _ = einsum(&[&tensor_a, &tensor_b], (["ij", "jk"], "ik"));
+    start.elapsed().as_nanos()
+}
+
+fn einsum4() -> u128 {
+    let i = 100;
+    let j = 1000;
+    let k = 500;
+
+    let tensor_a: Tensor<f32> = Tensor::rand([i, k]);
+    let tensor_b: Tensor<f32> = Tensor::rand([j, k]);
+
+    let start = ProcessTime::now();
+    _ = einsum(&[&tensor_a, &tensor_b], (["ik", "jk"], "ij"));
+    start.elapsed().as_nanos()
+}
+
+fn einsum5() -> u128 {
+    let a = 100;
+    let b = 5;
+    let c = 20;
+    let d = 50;
+    let e = 100;
+
+    let tensor_a: Tensor<f32> = Tensor::rand([a, b, c]);
+    let tensor_b: Tensor<f32> = Tensor::rand([b, d]);
+    let tensor_c: Tensor<f32> = Tensor::rand([d, e]);
+
+    let start = ProcessTime::now();
+    _ = einsum(&[&tensor_a, &tensor_b, &tensor_c], (["abc", "bd", "de"], "ae"));
+    start.elapsed().as_nanos()
+}
+
+fn einsum6() -> u128 {
+    let a = 100;
+    let b = 5;
+    let c = 20;
+    let d = 50;
+    let e = 100;
+
+    let tensor_a: Tensor<f32> = Tensor::rand([a, b, c]);
+    let tensor_b: Tensor<f32> = Tensor::rand([b, d]);
+    let tensor_c: Tensor<f32> = Tensor::rand([b, c]);
+    let tensor_d: Tensor<f32> = Tensor::rand([d, e]);
+
+    let start = ProcessTime::now();
+    _ = einsum(&[&tensor_a, &tensor_b, &tensor_c, &tensor_d], (["abc", "bd", "bc", "de"], "ae"));
+    start.elapsed().as_nanos()
+}
+
+fn einsum7() -> u128 {
+    let i = 128;
+    let j = 64;
+    let k = 32;
+
+    let tensor_a: Tensor<f32> = Tensor::rand([i, j]);
+    let tensor_b: Tensor<f32> = Tensor::rand([k, j]);
+
+    let start = ProcessTime::now();
+    _ = einsum(&[&tensor_a, &tensor_b], (["ij", "kj"], "ikj"));
+    start.elapsed().as_nanos()
+}
+
+fn einsum8() -> u128 {
+    let a = 128;
+    let b = 64;
+    let c = 32;
+
+    let tensor_a: Tensor<f32> = Tensor::rand([a, b, c]);
+
+    let start = ProcessTime::now();
+    _ = einsum(&[&tensor_a], (["abc"], ""));
+    start.elapsed().as_nanos()
+}
+
+fn einsum9() -> u128 {
+    let i = 1000;
+
+    let tensor_a: Tensor<f32> = Tensor::rand([i, i]);
+
+    let start = ProcessTime::now();
+    _ = einsum(&[&tensor_a], (["ii"], "i"));
+    start.elapsed().as_nanos()
+}
+
+fn einsum10() -> u128 {
+    let a = 10;
+    let b = 20;
+    let c = 30;
+    let d = 40;
+
+    let tensor_a: Tensor<f32> = Tensor::rand([a, b, c, d]);
+
+    let start = ProcessTime::now();
+    _ = einsum(&[&tensor_a], (["abcd"], "dcba"));
     start.elapsed().as_nanos()
 }
 
 fn main() {
-    println!("{}", profile());
+    let args: Vec<String> = env::args().collect();
+    let id = args[1].parse::<usize>().unwrap();
+
+    let time =
+        if id == 1 { einsum1() }
+        else if id == 2 { einsum2() }
+        else if id == 3 { einsum3() }
+        else if id == 4 { einsum4() }
+        else if id == 5 { einsum5() }
+        else if id == 6 { einsum6() }
+        else if id == 7 { einsum7() }
+        else if id == 8 { einsum8() }
+        else if id == 9 { einsum9() }
+        else if id == 10 { einsum10() }
+        else { panic!("invalid ID") };
+
+    println!("{}", time);
 }
