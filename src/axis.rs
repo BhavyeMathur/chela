37696b6ea774ad33e99dc34f36da63779e34b1a9
Bhavye Mathur <bhavyemathur@gmail.com>
2025-05-21 13:57:50 -0500

    Added get_absolute() method to AxisType

--- src/axis.rs
+++ src/axis.rs
@@ -4,6 +4,8 @@ pub(crate) mod indexer;
 pub mod index;
 #[macro_use]
 pub mod slice_index;
+
 pub mod axes_traits;
+pub use axes_traits::*;
 
 pub struct Axis(pub isize);
--- src/axis/axes_traits.rs
+++ src/axis/axes_traits.rs
@@ -3,6 +3,40 @@ use crate::Axis;
 
 pub trait AxisType {
     fn isize(&self) -> isize;
+
+    /// Computes the absolute axis index for a given tensor dimension.
+    /// 
+    /// Negative axis values are normalized to represent their positive counterparts.
+    /// For example, `-1` represents the last axis, `-2` the second-to-last axis, and so on.
+    ///
+    /// # Arguments
+    ///
+    /// * `ndims` - The total number of dimensions in the tensor.
+    ///
+    /// # Panics
+    ///
+    /// This function will panic if:
+    /// * The provided axis is less than `-ndims` (lower bound).
+    /// * The provided axis is greater than or equal to `ndims` (upper bound).
+    ///
+    /// # Examples
+    ///
+    /// ```rust
+    /// # use chela::*;
+    /// assert_eq!(Axis(-1).get_absolute(4), 3);
+    /// assert_eq!(Axis(-2).get_absolute(4), 2);
+    /// assert_eq!(Axis(1).get_absolute(4), 1);
+    /// ```
+    fn get_absolute(&self, ndims: usize) -> usize {
+        let axis = self.isize();
+        let ndims = ndims as isize;
+
+        if axis < -ndims || axis >= ndims {
+            panic!("axis '{}' out of bounds for tensor of dimension {}", axis, ndims);
+        }
+
+        (if axis < 0 { axis + ndims } else { axis }) as usize
+    }
 }
 
 impl AxisType for Axis {
